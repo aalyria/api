99d783db14b2bf9cffde2942c03db70d2722f0a5
Aalyria Technologies, Inc <noreply@aalyria.com>
2023-06-06 05:38:59 +0000

    Project import generated by Copybara.
    GitOrigin-RevId: 4b428f8a5b2d8fb7757111189dfcf0b83fba8ded

--- java/com/aalyria/spacetime/codesamples/nbi/client/ListEntities.java
+++ java/com/aalyria/spacetime/codesamples/nbi/client/ListEntities.java
@@ -12,70 +12,69 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.aalyria.spacetime.nbi.sample_client;
+package com.aalyria.spacetime.codesamples.nbi.client;
 
 import static io.grpc.Grpc.newChannelBuilderForAddress;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 import com.aalyria.spacetime.api.nbi.v1alpha.Nbi.EntityType;
 import com.aalyria.spacetime.api.nbi.v1alpha.Nbi.ListEntitiesRequest;
 import com.aalyria.spacetime.api.nbi.v1alpha.Nbi.ListEntitiesResponse;
 import com.aalyria.spacetime.api.nbi.v1alpha.NetOpsGrpc;
 import com.aalyria.spacetime.authentication.SpacetimeCallCredentials;
-
 import io.grpc.CompositeChannelCredentials;
 import io.grpc.ManagedChannel;
 import io.grpc.TlsChannelCredentials;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
- * A sample class that sets up the authentication parameters to call Spacetime's
- * Northbound Interface (NBI).
+ * A sample class that sets up the authentication parameters to call Spacetime's Northbound
+ * Interface (NBI).
  */
 public class ListEntities {
   private static final int PORT = 443;
 
   public static void main(String[] args) {
     if (args.length < 4) {
-        System.err.println("Error parsing arguments. Provide a value for host, " + 
-                "agent email, agent private key ID, and agent private key file.");
-        System.exit(-1);
+      System.err.println(
+          "Error parsing arguments. Provide a value for host, "
+              + "agent email, agent private key ID, and agent private key file.");
+      System.exit(-1);
     }
     final String HOST = args[0];
     final String AGENT_EMAIL = args[1];
     final String AGENT_PRIV_KEY_ID = args[2];
     final String AGENT_PRIV_KEY_FILE = args[3];
 
-    // The private key should start with "-----BEGIN RSA PRIVATE KEY-----" and 
+    // The private key should start with "-----BEGIN RSA PRIVATE KEY-----" and
     // end with "-----END RSA PRIVATE KEY-----". In between, there should be newline-delimtted
     // strings of characters.
     String privateKey;
     try {
-        privateKey = new String(Files.readAllBytes(Paths.get(AGENT_PRIV_KEY_FILE)));
+      privateKey = new String(Files.readAllBytes(Paths.get(AGENT_PRIV_KEY_FILE)));
     } catch (IOException e) {
-        throw new RuntimeException("Private key could not be read.", e);
+      throw new RuntimeException("Private key could not be read.", e);
     }
 
     // Sets up the channel using the two signed JWTs for RPCs to the NBI.
-    ManagedChannel channel = newChannelBuilderForAddress(
-        HOST,
-        PORT,
-        CompositeChannelCredentials.create(
-            TlsChannelCredentials.create(),
-            SpacetimeCallCredentials.createFromPrivateKey(HOST, AGENT_EMAIL,
-                AGENT_PRIV_KEY_ID, privateKey)))
-        .enableRetry()
-        .build();
+    ManagedChannel channel =
+        newChannelBuilderForAddress(
+                HOST,
+                PORT,
+                CompositeChannelCredentials.create(
+                    TlsChannelCredentials.create(),
+                    SpacetimeCallCredentials.createFromPrivateKey(
+                        HOST, AGENT_EMAIL, AGENT_PRIV_KEY_ID, privateKey)))
+            .enableRetry()
+            .build();
 
     // Sets up a stub to invoke RPCs against the NBI's NetOps service.
     NetOpsGrpc.NetOpsBlockingStub stub = NetOpsGrpc.newBlockingStub(channel);
 
     // This stub can now be used to call any method in the NetOps service.
-    ListEntitiesRequest request = ListEntitiesRequest.newBuilder()
-        .setType(EntityType.PLATFORM_DEFINITION.name())
-        .build();
+    ListEntitiesRequest request =
+        ListEntitiesRequest.newBuilder().setType(EntityType.PLATFORM_DEFINITION.name()).build();
     ListEntitiesResponse entities = stub.listEntities(request);
     System.out.println("ListEntitiesResponse received: \n" + entities.toString());
   }
---
+++ py/authentication/jwt_manager.py
@@ -0,0 +1,110 @@
+'''
+Copyright 2023 Aalyria Technologies, Inc., and its affiliates.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+import base64
+import json
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import serialization
+from datetime import timedelta, datetime
+
+
+class JwtManager:
+
+    def __init__(
+        self,
+        lifetime=timedelta(seconds=0),
+        audience="",
+        issuer="",
+        subject="",
+        target_audience="",
+        private_key_id="",
+        private_key="",
+        jwt_value="",
+    ):
+        self.lifetime = lifetime
+        self.audience = audience
+        self.issuer = issuer
+        self.subject = subject if subject else issuer
+        self.target_audience = target_audience
+        self.private_key_id = private_key_id
+        self.private_key = private_key
+        self.jwt_value = jwt_value if jwt_value else self.generate_jwt()
+
+    def generate_jwt(self) -> str:
+        private_key = self.decode_private_key()
+        header = self.generate_header()
+        payload = self.generate_payload()
+        signature = self.generate_signature(header, payload, private_key)
+
+        jwt = b".".join([header, payload, signature])
+        jwt_value = jwt.decode()
+        self.jwt_value = jwt_value
+        return jwt_value
+
+    def decode_private_key(self) -> rsa.RSAPrivateKey:
+        private_key_pem = self.private_key.encode("utf-8")
+        try:
+            private_key = serialization.load_pem_private_key(
+                private_key_pem, None)
+        except (ValueError, TypeError) as e:
+            raise ValueError("Invalid private key.") from e
+
+        return private_key
+
+    def generate_header(self) -> bytes:
+        header = {
+            "alg": "RS256",
+            "typ": "JWT",
+            "kid": self.private_key_id,
+        }
+        encoded_header = json.dumps(header).encode()
+        return base64.urlsafe_b64encode(encoded_header)
+
+    def generate_payload(self) -> bytes:
+        now = datetime.utcnow()
+        issue_time = int(now.timestamp())
+        expiration_time = int((now + self.lifetime).timestamp())
+
+        payload = {
+            "aud": self.audience,
+            "exp": expiration_time,
+            "iat": issue_time,
+            "iss": self.issuer,
+            "sub": self.subject,
+        }
+        if self.target_audience:
+            payload["target_audience"] = self.target_audience
+
+        encoded_payload = json.dumps(payload).encode()
+        return base64.urlsafe_b64encode(encoded_payload)
+
+    def generate_signature(
+        self,
+        header: bytes,
+        payload: bytes,
+        private_key: rsa.RSAPrivateKey,
+    ) -> bytes:
+        message = b".".join([header, payload])
+        signature = private_key.sign(
+            message,
+            padding.PKCS1v15(),
+            hashes.SHA256(),
+        )
+        return base64.urlsafe_b64encode(signature)
+
+    def get_jwt(self) -> str:
+        return self.jwt_value
