5d81b1850efdf95061f7fd1d3be0bcfa1a4b9b70
Aalyria Technologies, Inc <noreply@aalyria.com>
2024-08-26 21:22:21 +0000

    Import changes.
      - d23848601c13ca326ee0eee83c0b6db03e0d4bd2
    
    GitOrigin-RevId: d23848601c13ca326ee0eee83c0b6db03e0d4bd2

--- tools/gopackagesdriver.sh
+++ tools/gopackagesdriver.sh
@@ -14,4 +14,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-exec bazel run -- @io_bazel_rules_go//go/tools/gopackagesdriver "${@}"
+exec bazel run -- @rules_go//go/tools/gopackagesdriver "${@}"
